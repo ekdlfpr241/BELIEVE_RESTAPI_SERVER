dab60663e5cd2c1459a881476f7a51961a04e3a5
KJH <ekdlfpr241@naver.com>
2016-11-28 00:30:44 +0900

    Match,GroupMatch Routing Added

---
+++ routes/Match/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var pgInstance = require('../../module/database');
+var router = express.Router();
+
+/* 매칭 관련 REST */
+
+/* 파라미터가 없는 SELECT 문 REST처리 */
+router.get('/getCondition', function(req, res, next) {
+
+});
+
+
+module.exports = router;
